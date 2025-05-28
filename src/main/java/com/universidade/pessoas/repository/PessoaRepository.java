234d3b387c7fbf30e9e5eba92525f399a98d8883
Vinicius Emanuel <150941917+MrLonely0-0@users.noreply.github.com>
2025-05-28 08:14:33 -0300

    Update PessoaRepository.java

--- src/main/java/com/universidade/pessoas/repository/PessoaRepository.java
+++ src/main/java/com/universidade/pessoas/repository/PessoaRepository.java
@@ -1,11 +1,13 @@
-package com.universidade.pessoas.repository;
+package com.faculdade.pessoa.repository;
 
-import com.universidade.pessoas.model.Pessoa;
+import com.faculdade.pessoa.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface PessoaRepository extends JpaRepository<Pessoa, UUID>{
+public interface PessoaRepository extends JpaRepository<Pessoa, UUID> {
+    Optional<Pessoa> findByCpf(String cpf);
 }
