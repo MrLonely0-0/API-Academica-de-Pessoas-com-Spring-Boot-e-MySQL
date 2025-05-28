b4c619cef6b69af8491edd93f824dc7e8ae5c84c
Vinicius Emanuel <150941917+MrLonely0-0@users.noreply.github.com>
2025-05-28 00:07:22 -0300

    Create Curso.java
    ;-;

---
+++ src/main/java/com/universidade/model/Curso.java
@@ -0,0 +1,23 @@
+package com.universidade.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+}
