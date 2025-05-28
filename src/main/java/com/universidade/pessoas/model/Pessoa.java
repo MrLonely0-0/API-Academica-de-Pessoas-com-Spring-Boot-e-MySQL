339dcb49be647905e257131184d1c901e6837ba4
Vinicius Emanuel <150941917+MrLonely0-0@users.noreply.github.com>
2025-05-28 00:24:08 -0300

    Update Pessoa.java

--- src/main/java/com/universidade/pessoas/model/Pessoa.java
+++ src/main/java/com/universidade/pessoas/model/Pessoa.java
@@ -1,31 +1,46 @@
-package com.universidade.pessoas.model;
+package com.universidade.pessoa.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-
+import lombok.*;
 import java.util.UUID;
+import java.util.Set;
 
 @Entity
-@Table(name = "universidade")
+@Table(name = "pessoas")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pessoa {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false, unique = true, length = 11)
     private String cpf;
 
+    @Column(nullable = false)
     private int age;
 
+    // Relacionamento com Curso (Muitos para Muitos)
+    @ManyToMany
+    @JoinTable(
+        name = "pessoa_curso",
+        joinColumns = @JoinColumn(name = "pessoa_id"),
+        inverseJoinColumns = @JoinColumn(name = "curso_id")
+    )
+    private Set<Curso> cursos;
+
     @Override
-    public String toString(){
+    public String toString() {
         return "Pessoa{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", cpf='" + cpf + '\'' +
-                ", age='" + age + '\'' +
+                ", age=" + age +
                 '}';
     }
 }
