d02bbdda8cc03f293052e7575b63d8d5a94dbb86
Vinicius Emanuel <150941917+MrLonely0-0@users.noreply.github.com>
2025-05-28 08:46:28 -0300

    Update PessoaDTO.java

--- src/main/java/com/universidade/pessoas/dto/PessoaDTO.java
+++ src/main/java/com/universidade/pessoas/dto/PessoaDTO.java
@@ -1,21 +1,20 @@
-package com.universidade.dto;
+package com.universidade.pessoa.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class PessoaDTO {
 
-    @NotBlank
+    @NotBlank(message = "Nome é obrigatório")
     private String nome;
 
-    @NotBlank
+    @NotBlank(message = "CPF é obrigatório")
+    @Pattern(regexp = "\\d{11}", message = "CPF deve conter 11 dígitos")
     private String cpf;
 
-    @NotBlank
-    private String email;
-
-    @NotNull(message = "O ID do curso é obrigatório")
-    private Long cursoId;
+    @Min(value = 18, message = "Idade mínima é 18 anos")
+    private int idade;
 }
